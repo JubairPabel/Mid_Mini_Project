789cabef96a7a5643964d8403e87001a6189680b
Jubair Uddin Pabel <gtxbiscuit@gmail.com>
2020-08-09 10:50:31 +0600

    1

--- RegCheck.php
+++ RegCheck.php
@@ -1,8 +1,11 @@
 <?php
-	
+	session_start();
+
+
 
 	if(isset($_POST['submit'])){
 
+    
 		$id = $_POST['ID'];
 		$password = $_POST['Password'];
 		$name = $_POST['Name'];
@@ -10,19 +13,15 @@
 		$type = $_POST['Type'];
 
 
+         
+
 
 		if(empty($id) || empty($password) || empty($name) || empty($email) || empty($type)){
 			echo "null submission";
 		}else {
 
-		
-	
-
-
-			$file = fopen('user.txt', 'a');
-			fwrite($file, $id.'|'.$password.'|'.$name. '|' .$email. '|' .$type."/r/n");
-			fclose($file);
-
+	    	$conn = mysqli_connect('127.0.0.1', 'root', '', 'webtech');
+	        $result = mysqli_query($conn, 'Insert into user values ('$id','$password','$name','$email','$type') ');
 
 
 			header('location: Login.html');
